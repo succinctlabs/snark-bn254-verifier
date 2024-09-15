c31e1a829c2d23af0cdd7a3ee107edf0d96c7f54
Bhargav Annem <bhargav.annem@gmail.com>
2024-09-14 19:21:06 -0700

    fix: path

--- examples/program/src/main.rs
+++ examples/program/src/main.rs
@@ -5,10 +5,10 @@ use snark_bn254_verifier::PlonkVerifier;
 use substrate_bn::Fr;
 
 pub fn main() {
-    let proof = sp1_zkvm::io::read_vec();
-    let vk = sp1_zkvm::io::read_vec();
-    let vkey_hash = sp1_zkvm::io::read_vec();
-    let committed_values_digest_bytes = sp1_zkvm::io::read_vec();
+    let proof = sp1_zkvm::io::read_vec()[8..].to_vec();
+    let vk = sp1_zkvm::io::read_vec()[8..].to_vec();
+    let vkey_hash = sp1_zkvm::io::read_vec()[8..].to_vec();
+    let committed_values_digest_bytes = sp1_zkvm::io::read_vec()[8..].to_vec();
 
     let proof = proof;
     let vk = vk;
