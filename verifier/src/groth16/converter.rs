15cc75c37c359a33154f5ba6d01feeb0c87a74ca
Bhargav Annem <bhargav.annem@gmail.com>
2024-09-16 11:21:49 -0700

    refactor: converters

--- verifier/src/groth16/converter.rs
+++ verifier/src/groth16/converter.rs
@@ -1,82 +1,11 @@
-use bn::{AffineG1, AffineG2, Fq, Fq2};
-use core::cmp::Ordering;
+use bn::AffineG1;
 
 use crate::{
-    constants::{CompressedPointFlag, COMPRESSED_INFINITY, MASK},
-    converter::is_zeroed,
-    error::Error,
+    converter::{compressed_x_to_g1_point, compressed_x_to_g2_point},
     groth16::{Groth16G1, Groth16G2, Groth16Proof, Groth16VerifyingKey, PedersenVerifyingKey},
 };
 
 use super::error::Groth16Error;
-
-fn deserialize_with_flags(buf: &[u8]) -> Result<(Fq, CompressedPointFlag), Groth16Error> {
-    if buf.len() != 32 {
-        return Err(Groth16Error::GeneralError(Error::InvalidData));
-    };
-
-    let m_data = buf[0] & MASK;
-    if m_data == COMPRESSED_INFINITY {
-        if !is_zeroed(buf[0] & !MASK, &buf[1..32])? {
-            return Err(Groth16Error::GeneralError(Error::InvalidData));
-        }
-        Ok((Fq::zero(), CompressedPointFlag::Infinity))
-    } else {
-        let mut x_bytes: [u8; 32] = [0u8; 32];
-        x_bytes.copy_from_slice(buf);
-        x_bytes[0] &= !MASK;
-
-        let x = Fq::from_be_bytes_mod_order(&x_bytes.to_vec())
-            .expect("Failed to convert x bytes to Fq");
-
-        Ok((x, CompressedPointFlag::from(m_data)))
-    }
-}
-
-fn compressed_x_to_g1_point(buf: &[u8]) -> Result<AffineG1, Groth16Error> {
-    let (x, m_data) = deserialize_with_flags(buf)?;
-    let (y, neg_y) = AffineG1::get_ys_from_x_unchecked(x)
-        .ok_or(Groth16Error::GeneralError(Error::FailedToGetY))?;
-
-    let mut final_y = y;
-    if y.cmp(&neg_y) == Ordering::Greater {
-        if m_data == CompressedPointFlag::Positive {
-            final_y = -y;
-        }
-    } else {
-        if m_data == CompressedPointFlag::Negative {
-            final_y = -y;
-        }
-    }
-
-    Ok(AffineG1::new(x, final_y).map_err(|_| Error::SerializationError)?)
-}
-
-fn compressed_x_to_g2_point(buf: &[u8]) -> Result<AffineG2, Groth16Error> {
-    if buf.len() != 64 {
-        return Err(Groth16Error::GeneralError(Error::InvalidData));
-    };
-
-    let (x0, _) = deserialize_with_flags(&buf[..32])?;
-    let (x1, flag) = deserialize_with_flags(&buf[32..64])?;
-    let x = Fq2::new(x0, x1);
-
-    if flag == CompressedPointFlag::Infinity {
-        return Ok(AffineG2::one());
-    }
-
-    let (y, neg_y) = AffineG2::get_ys_from_x_unchecked(x).ok_or(Error::SerializationError)?;
-
-    match flag {
-        CompressedPointFlag::Positive => {
-            Ok(AffineG2::new(x, y).map_err(|_| Error::SerializationError)?)
-        }
-        CompressedPointFlag::Negative => {
-            Ok(AffineG2::new(x, neg_y).map_err(|_| Error::SerializationError)?)
-        }
-        _ => Err(Groth16Error::GeneralError(Error::InvalidData)),
-    }
-}
 
 pub(crate) fn load_groth16_proof_from_bytes(buffer: &[u8]) -> Result<Groth16Proof, Groth16Error> {
     let ar = compressed_x_to_g1_point(&buffer[..32])?;
